82ab9fecdfc28e16facebe753c4f8370763d49c9
Melkchri100 <melkchri100@student.kau.se>
2024-10-23 15:40:51 +0200

    added enterCredentials class that takes user input

--- src/labben/labben/LoginIF.java
+++ src/labben/labben/LoginIF.java
@@ -1,7 +1,36 @@
 package labben;
 
+import java.util.Scanner;
+
 public class LoginIF {
 	public void enterCredentials() {
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println(
+				"Welcome to ChatterBox.\nPress 1 to login\nPress 2 to stay as guest\nPress anything else to quit application");
+
+		try {
+			int choice = scan.nextInt();
+			scan.nextLine();
+
+			if (choice == 1) {
+				System.out.println("Enter username: ");
+				String username = scan.nextLine();
+
+				System.out.println("Enter password: ");
+				String password = scan.nextLine();
+
+			} else if (choice == 2) {
+				System.out.println("Continuing as a guest...");
+			} else {
+				System.out.println("Quitting application. Goodbye!");
+				System.exit(0);
+			}
+		} catch (Exception e) {
+			// If input is not an integer
+			System.out.println("Quitting application. Goodbye!");
+			System.exit(0);
+		} finally {
+			scan.close();
+		}
 	}
 }
--- src/labben/labben/Main.java
+++ src/labben/labben/Main.java
@@ -3,6 +3,8 @@ package labben;
 public class Main {
 	public static void main(String[] args) {
 	    System.out.println("Hejsan");
+	    LoginIF loginIF = new LoginIF();
+		loginIF.enterCredentials();
 		}
 }
 
