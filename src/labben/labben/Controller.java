400e4ff8bc9900c0812ce6f55663e9c368544afd
Melkchri100 <melkchri100@student.kau.se>
2024-10-29 12:36:24 +0100

    fixed userChoices

--- src/labben/labben/Controller.java
+++ src/labben/labben/Controller.java
@@ -9,37 +9,13 @@ public class Controller {
 		Scanner scan = new Scanner(System.in);
 		if (correctLogin) {
 			System.out.println("Correct login");
-			System.out.println("Welcome user\nPress 1 to view all messages\nPress 2 to view your messages\n"
-					+ "Press 3 to create Post\nPress anything else to quit");
-			try {
-				int choice = scan.nextInt();
-				scan.nextLine();
-				if (choice == 1) {
-					Posts.initializePosts();
-					Posts.viewAllPosts();
-				} else if (choice == 2) {
-					Posts.initializePosts();
-					Posts.viewOwnPost(username);
-				} else if (choice == 3) {
-					//CreatePostIF.enterMessage();
-				}
-				else {
-					System.out.println("Quitting application. Goodbye!");
-					System.exit(0);
-				}
-
-			} catch (Exception e) {
-				// If input is not an integer
-				System.out.println("Quitting application. Goodbye!");
-				System.exit(0);
-			}
-			
-			
-		} else {
-			System.out.println("Incorrect login");
+			Posts.initializePosts();
+			User.userChoices(username, password);
+		}
+	    else {
+		System.out.println("Incorrect login");
 		}
 	}
-
 	public void verifyPost() {
 
 	}
--- src/labben/labben/User.java
+++ src/labben/labben/User.java
@@ -1,5 +1,7 @@
 package labben;
 
+import java.util.Scanner;
+
 public class User {
 	String username;
 	String password;
@@ -7,6 +9,35 @@ public class User {
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
+	}
+
+	public static void userChoices(String username, String password) {
+		System.out.println("Welcome user\nPress 1 to view all messages\nPress 2 to view your messages\n"
+				+ "Press 3 to create Post\nPress anything else to quit");
+		Scanner scan = new Scanner(System.in);
+		try {
+			int choice = scan.nextInt();
+			scan.nextLine();
+			if (choice == 1) {
+				Posts.viewAllPosts();
+				User.userChoices(username, password);
+			} else if (choice == 2) {
+				Posts.viewOwnPost(username);
+				User.userChoices(username, password);
+			} else if (choice == 3) {
+				// CreatePostIF.enterMessage();
+				User.userChoices(username, password);
+			} else {
+				System.out.println("Quitting application. Goodbye!");
+				System.exit(0);
+			}
+
+		} catch (Exception e) {
+			// If input is not an integer
+			System.out.println("Quitting application. Goodbye!");
+			System.exit(0);
+		}
+
 	}
 
 }
