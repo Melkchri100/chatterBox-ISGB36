4304468da44306c2cafb0002087d34ea5ce91134
DeddTecc <melker.westin@gmail.com>
2024-10-29 14:25:06 +0100

    bingus

--- src/labben/labben/Posts.java
+++ src/labben/labben/Posts.java
@@ -2,18 +2,36 @@ package labben;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.io.*;
 
 public class Posts {
 	public static List<Post> postList = new ArrayList<>();
 
 	public static void initializePosts() {
-
+		Scanner input = null;
+		String file ="posts.csv";
+		
+		try {
+			input = new Scanner(new File(file));
+			
+			while(input.hasNextLine()) {
+				String line = input.nextLine();
+				String [] toks = line.split("\t");
+				String username = toks [0].trim();
+				String message = toks [1].trim();
+				int postID = Integer.parseInt(toks [2].trim());
+				
+				postList.add(new Post(username,message,postID));
+			}
+		 }
+		
+		
 		postList.add(new Post("jnash", "Hello World!", 1));
 		postList.add(new Post("akim", "Hello bro", 2));
 		postList.add(new Post("jnash", "How do you like your cheese?", 3));
 		postList.add(new Post("akim", "Drippy bruh", 4));
 		postList.add(new Post("jnash", "Nice pookie bear ❤", 5));
-
 	}
 
 	public void linkUser() {
